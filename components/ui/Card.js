9666c381ddbcf18b6eb4500c1f3d47f5e0ac19fb
CHAN Ho Long <roychanho@gmail.com>
2023-01-18 00:01:15 +0800

    update

---
+++ components/ui/Card.js
@@ -0,0 +1,30 @@
+import { Dimensions, StyleSheet, View } from "react-native";
+import React from "react";
+import Colors from "../../constants/colors";
+
+const Card = ({ children }) => {
+  return <View style={styles.inputContainer}>{children}</View>;
+};
+
+export default Card;
+
+const deviceWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: deviceWidth < 380 ? 18 : 36,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    borderRadius: 8,
+    // for android
+    elevation: 4,
+    // for iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
